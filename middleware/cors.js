ecf80d2abb2308f00b632d71fea0a047fb94d4dd
javigarcias <javigarciaweb@gmail.com>
2021-10-12 00:31:16 +0200

    Actualizo cors

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-API-KEY, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, DELETE");
+    res.header('Access-Control-Allow-Credentials', true);
     next();
 }
